01e4e3524e6645cad2dd39d2b28df2c97138fe84
Yuri <yurilp.bandeira@gmail.com>
2025-12-23 19:28:58 -0300

    Refactor getters and method names in Personagem class

--- RPG/Personagem.java
+++ RPG/Personagem.java
@@ -10,15 +10,15 @@ public class Personagem{
         this.ataqueBase=ataqueBase;
         this.defesaBase=defesaBase;
     }
-    public String getnome(){return nome;}
-    public int getvida(){return vida;}
-    public int getvidaMaxima(){return vidaMaxima;}
-    public int getataqueBase(){return ataqueBase;}
-    public int getdefesaBase(){return defesaBase;}
-    public List<Item>getinventario(){return inventario;}
-    public List<Status>getstatus(){return statusAtivo;}
+    public String getNome(){return nome;}
+    public int getVida(){return vida;}
+    public int getVidaMaxima(){return vidaMaxima;}
+    public int getAtaqueBase(){return ataqueBase;}
+    public int getDefesaBase(){return defesaBase;}
+    public List<Item>getInventario(){return inventario;}
+    public List<Status>getStatus(){return statusAtivo;}
     //receber dano//
-    public void receberdano(int dano){
+    public void receberDano(int dano){
         this.vida-=dano;
         if(this.vida<0){this.vida=0;}
         System.out.println(this.nome+" recebeu "+dano+" de dano. Vida restante: "+this.vida);
@@ -34,5 +34,35 @@ public class Personagem{
         for(status : statusAtivo){
             if(status.gettipo().equals("Queimadura")){System.out.println(this.nome+" está queimado! E recebeu "+status.getDanoPorTurno()+" de dano de queimadura.");}
         }
+    public void cura(int quantidadeDeCura){
+        this.vida+=this.quantidade_cura;
+        if(this.vida>this.vidaMaxima){
+            this.vida=this.vidaMaxima;
+        }
+        System.out.println(this.nome+" curou "+ this.quantidade_Cura+" de vida. Vida atual: "+this.vida);
+    }
+    public void ataque(personagem Alvo){
+        int dano=ataqueBase-alvo.defesaBase;
+        if(dano<0){
+            dano=0;
+            System.out.println(this.nome+" atacou "+alvo.nome+" e causou "+dano+" de dano.");
+            alvo.receberDano(dano);
+        }
+    public void exibirStatus(){
+        System.out.println("==="+this.nome+"===");
+        System.out.println("Vida: "+this.vida+"/"+this.vidaMaxima);
+        System.out.println("Ataque: "+this.ataque);
+        System.out.println("Defesa: "+this.vida);
+        System.out.println("Mana: "+this.mana+"/"+this.manaMaxima);
+        if(!statusAtivos.isempty()){
+            System.out.println("Status: ");
+            for(status s:statusAtivos){
+                System.out.println(s.getTipo()+"("s.getDuracao()+turnos")");
+            }
+        }
+        public boolean estaVivo(){
+            return this.vida>0;
+        }
+    }
     }
 }
